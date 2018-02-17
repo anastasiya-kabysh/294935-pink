62a0c227be97e4260523760c664f471ffe05384f
anastasia-kabysh <anastasiya.kabysh@gmail.com>
2018-02-17 17:45:23 +0100

    Desktop pixel perfect completed

---
+++ source/js/app.js
@@ -0,0 +1,25 @@
+(function() {
+  var btns = document.querySelectorAll('.js-toggle');
+  var menu = document.getElementById('nav');
+  var header = document.getElementById('header');
+  var btnsForm = document.querySelectorAll('.btn--form');
+
+  header.classList.add('page-header--js');
+  header.classList.remove('page-header--active');
+  menu.classList.add('navigation--closed');
+  menu.classList.add('navigation--positioned');
+
+  handleMenu(menu, btns);
+  handleButtons(btnsForm);
+  svg4everybody();
+})();
+
+function preview() {
+  var preview = document.querySelector('.preview');
+  var preloader = document.querySelector('#preloader');
+
+  preloader.classList.remove('js-show');
+  preview.classList.remove('preview');
+}
+
+window.onload = preview;
